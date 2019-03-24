ee88314ecfcc08ed856b6a6c289880499b5c51b0
Väinö Järvelä <vaino@speechgrinder.com>
2019-03-24 08:36:14 +0200

    Cleanup macos.el

--- site-lisp/macos.el
+++ site-lisp/macos.el
@@ -1,14 +1,10 @@
 ;;; macos -*- lexical-binding: t -*-
-(setq mac-pass-option-to-system nil
-      mac-pass-control-to-system nil
+(setq mac-pass-control-to-system nil
       mac-pass-command-to-system nil
-      mac-command-key-is-meta t
-      mac-option-key-is-meta nil
       mac-command-modifier 'meta
       mac-option-modifier 'control
       mac-control-modifier 'control
       mac-function-modifier 'meta
-      browse-url-browser-function 'browse-url-default-macosx-browser
-      ns-use-native-fullscreen nil)
+      browse-url-browser-function 'browse-url-default-macosx-browser)
 
 (provide 'macos)
