00c657fcd0410bf7a8dd7e2e7e5e9fccea703f3f
Väinö Järvelä <vaino@jarve.la>
2019-07-18 10:42:29 +0300

    Use emoji on macOS with Emacs >= 27.0

--- site-lisp/macos.el
+++ site-lisp/macos.el
@@ -8,4 +8,7 @@
       mac-mouse-wheel-smooth-scroll nil
       browse-url-browser-function 'browse-url-default-macosx-browser)
 
+(unless (version< "27.0" emacs-version)
+    (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") nil 'prepend))
+
 (provide 'macos)
